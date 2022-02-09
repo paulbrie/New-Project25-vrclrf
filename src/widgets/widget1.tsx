d05fd379d18107b8df5f77a10ac067ec21788ab5
Paul BRIE <aelythe@gmail.com>
2022-02-09 15:54:01 +0200

    add demo line

--- src/widgets/widget1.tsx
+++ src/widgets/widget1.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+// new line, just for fun
+
 const Widget1 = ({ title, color }: { title: string, color: string }) => {
     return (<div style={{ color: color || '' }}>{title}</div>)
 }
